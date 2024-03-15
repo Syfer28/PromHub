2f44481b01c18b2b8dab2d0cda03156dde8ae87a
ElbekRahmanov@yandex.ru <ElbekRahmanov@yandex.ru>
2024-03-15 18:19:43 +0200

    commit

--- App.js
+++ App.js
@@ -1,1 +1,28 @@
-es;
+import MainNavigator from "./components/main/MainNavigator";
+import SignIn from "./components/enter/SignIn";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* <Stack.Screen name='SignIn' component={SignIn} /> */}
+        <Stack.Screen name="Main" component={MainNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
